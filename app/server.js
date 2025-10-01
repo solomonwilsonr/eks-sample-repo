0c018686a595f1be078e75480bafc6c61089bbb9
Solomon Wilson <Solomon.wilson@cdw.com>
2025-10-01 13:54:33 +0530

    first commit1

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
-const msg = process.env.MESSAGE || 'Hello from EKS CI/CD!';
+const msg = process.env.MESSAGE || 'Auto-deployed via CodePipeline!';
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type':'text/plain'});
   res.end(`${msg}\n`);
